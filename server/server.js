80eaee5235b95983a764b797980c613d0ac9a775
MSI <141185906+hieung88@users.noreply.github.com>
2024-01-19 11:52:38 -0600

    Added auth under utils, add route in server.js, set up in typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { authMiddleware } = require('./utils/auth');
 
 // Middleware
 app.use(express.json());
+app.use('/graphql', expressMiddleware(server, {
+  context: authMiddleware
+}));
 
 // Homepage route
 app.get('/', (req, res) => {
