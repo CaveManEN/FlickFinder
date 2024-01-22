badc576b8b4c68f1426c4039a86007d5a38f8ee4
Luis Mansilla <l_mansilla92@yahoo.com>
2024-01-22 01:45:33 -0600

    Added additional dependencies, created server side API fetch to provide client with data, created React routes, added additional css styling for result container and movie card components, created Error page component, created Header, Results, and SearchBar components. User input search form now functions and returns data from the server API fetch request and renders results to the page.

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <>
+        {/* Render content specific to homepage depending on if user is logged in or not */}
+        </>
+    )
+}
+
+export default HomePage;
