f88349fef513df0adff59840a5a8a83920430e19
Bubble <kiana.sambursky@gmail.com>
2024-01-25 21:09:28 -0600

    fixed errors

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -14,3 +14,22 @@ export default function ErrorPage() {
     </div>
   );
 }
+
+//xpert assistant said we need to use useLocation  
+
+// import React from 'react';
+// import { useLocation } from 'react-router-dom';
+
+// export default function ErrorPage() {
+//   const location = useLocation();
+
+//   return (
+//     <div id="error-page">
+//       <h1 id="oops">Oops!</h1>
+//       <p id="sorry">Sorry, an unexpected error has occurred.</p>
+//       <p>
+//         <i id="error">{location.state?.error || 'Unknown error'}</i>
+//       </p>
+//     </div>
+//   );
+// }
