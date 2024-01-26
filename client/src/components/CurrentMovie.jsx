cb31e40adc02e5edc081dd196ec6c26a1295d3d8
Luis Mansilla <l_mansilla92@yahoo.com>
2024-01-26 00:34:10 -0600

    Added director, writers, cast, and embedded movie trailer inside CurrentMovie component page

--- client/src/components/CurrentMovie.jsx
+++ client/src/components/CurrentMovie.jsx
@@ -6,12 +6,6 @@ const CurrentMovie = () => {
     const { data, setData } = useContext(AppContext);
     console.log('current movie data: ', data);
 
-    // if(data.titleText.text) {
-    //     const title = <h1>{data.titleText.text}</h1>
-    // }
-
-    // return <h1>{data.titleText.text}</h1>
-
     return (
         <div className='results-container'>
             <div className='current-movie-card'>
@@ -27,18 +21,30 @@ const CurrentMovie = () => {
                     <img
                         className='current-movie-img'
                         src={data.primaryImage.url}
+                        alt={`Poster for ${data.originalTitleText.text}`}
                     />
                     <section className='trailer-container'>
                         {/* embedded movie trailer */}
+                        {/* data.primaryVideo.edges[0].node.playbackURLs[0].url */}
+                        <iframe
+                            className='iframe'
+                            title="IMDb Video"
+                            width="560"
+                            height="315"
+                            src={data.primaryVideos.edges[0].node.playbackURLs[0].url}
+                            autoplay
+                            allowFullScreen
+                        ></iframe>
                     </section>
                 </section>
                 <section className='card-info'>
                     <p>{data.plot.plotText.plainText}</p>
                     {/* data.directors */}
                     {/* Need to figure out how to go through nested array of objects */}
-                    <p>DIRECTORS</p>
-                    <p>WRITERS</p>
-                    <p>CAST</p>
+                    <hr></hr>
+                    <p>Director: {data.directors[0].credits[0].name.nameText.text}</p>
+                    <p>Writers: {data.writers[0].credits.map((writer) => writer.name.nameText.text + ' \u00B7 ' )}</p>
+                    <p>Stars: {data.principalCredits[2].credits.map((star) => star.name.nameText.text + ' \u00B7 ')}</p>
                 </section>
             </div>
         </div>
