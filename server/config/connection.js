e28faf6cc13c576502dd79094c45014b63fdccc3
CaveManEN <ianccody@gmail.com>
2024-01-30 14:42:56 -0600

    trying to get render to work having db issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/flick-finder');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/flick-finder', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db; // Export the db object
